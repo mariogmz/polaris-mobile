68741e6fcb49dd6ac07f70e5a66e5e50d7808f95
Jaime Valdivia <jaimevaldivia@outlook.com>
2014-05-29 18:30:05 -0500

    Welcome screen prototype and Log in layout implemented.

--- src/com/evologics/polaris/MainActivity.java
+++ src/com/evologics/polaris/MainActivity.java
@@ -1,9 +1,12 @@
 package com.evologics.polaris;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void doCreateAccount(View view){
+    	//TODO: Create account implementation
+    	Toast.makeText(getApplicationContext(), "Create function called", Toast.LENGTH_LONG).show();
+    }
+    
+    public void doLogin(View view){
+    	Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+    	startActivity(intent);
     }
 }
