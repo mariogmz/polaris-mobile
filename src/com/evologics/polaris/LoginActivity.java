2244f0d02f1ba6336b01bbb52da0d5295e3a04cd
Jaime Valdivia <jaimevaldivia@outlook.com>
2014-06-04 16:41:15 -0500

    Testing

--- src/com/evologics/polaris/LoginActivity.java
+++ src/com/evologics/polaris/LoginActivity.java
@@ -29,7 +29,7 @@ import android.support.v4.app.NavUtils;
 public class LoginActivity extends Activity {
 	/**
 	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
+	 * TODO: remove after connecting to a real authentication system. 
 	 */
 	private static final String[] DUMMY_CREDENTIALS = new String[] {
 			"foo@example.com:hello", "bar@example.com:world" };
